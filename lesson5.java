3337f5ca1104622a6a05148f2bfb4e02b19f769a
Bliss Singhal <54075580+bluelilly1@users.noreply.github.com>
2021-08-01 20:28:24 -0700

    Create lesson5.java

---
+++ lesson5.java
@@ -0,0 +1,26 @@
+class Main {
+  int numberOfPets;
+  int animals;
+  //define more attributes
+
+
+  //make number and pets as parameters
+  //have the function return number of pets 
+  public void printPet() {
+    System.out.println("She has " + number + pets + ".");
+  }
+
+  //make another function that takes the return value of the previous function and does some mathmatical calculations with it and prints out the result
+
+  //make a private function that takes in some parameters and returns something
+  
+  public static void main(String[] args) {
+    //make an object
+
+    //define their attributes
+
+    //call all the functions and pass the at least some object attributes as parameters
+
+  }
+
+}
